aed476d3be95b1ca84617b3e0e76bc1b32b8e720
ddionisio <garapata@gmail.com>
2018-12-18 15:21:30 -0500

    added Signal Actions

---
+++ Actions/Signal/SignalInvoke.cs
@@ -0,0 +1,18 @@
+﻿using M8;
+
+namespace HutongGames.PlayMaker.Actions.M8 {
+    [ActionCategory("Mate Signal")]
+    [Tooltip("Invoke given signal.")]
+    public class SignalInvoke : FsmStateAction {
+        [ObjectType(typeof(Signal))]
+        [RequiredField]
+        public FsmObject signal;
+
+        public override void OnEnter() {
+            var s = (Signal)signal.Value;
+            s.Invoke();
+
+            Finish();
+        }
+    }
+}
---
+++ Actions/Signal/SignalWait.cs
@@ -0,0 +1,30 @@
+﻿using M8;
+
+namespace HutongGames.PlayMaker.Actions.M8 {
+    [ActionCategory("Mate Signal")]
+    [Tooltip("Wait for given signal.")]
+    public class SignalWait : FsmStateAction {
+        [ObjectType(typeof(Signal))]
+        [RequiredField]
+        public FsmObject signal;
+
+        public FsmEvent waitEndEvent;
+
+        private Signal mSignal;
+
+        public override void OnEnter() {
+            mSignal = (Signal)signal.Value;
+            mSignal.callback += OnSignal;
+        }
+
+        public override void OnExit() {
+            if(mSignal)
+                mSignal.callback -= OnSignal;
+        }
+
+        void OnSignal() {
+            Fsm.Event(waitEndEvent);
+            Finish();
+        }
+    }
+}

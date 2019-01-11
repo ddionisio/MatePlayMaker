8ec67784aa2eacbc85e624a3dc503f81322583fd
ddionisio <garapata@gmail.com>
2019-01-11 11:34:05 -0500

    added action SetAngularVelocity for Rigidbody2D

---
+++ Actions/Rigidbody2D/SetAngularVelocity.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.M8._Rigidbody2D {
+    [ActionCategory("Mate Rigidbody2D")]
+    [Tooltip("Set Rigidbody2D.angularVelocity")]
+    public class SetAngularVelocity : ComponentAction<Rigidbody2D> {
+        [RequiredField]
+        [CheckForComponent(typeof(Rigidbody2D))]
+        [Tooltip("The Rigidbody2D source.")]
+        public FsmOwnerDefault gameObject;
+
+        public FsmFloat angle;
+
+        public FsmBool everyFrame;
+
+        public override void Reset() {
+            gameObject = null;
+
+            angle = null;
+
+            everyFrame = false;
+        }
+
+        public override void Awake() {
+            Fsm.HandleFixedUpdate = true;
+        }
+
+        public override void OnEnter() {
+            ApplyValue();
+
+            if(!everyFrame.Value)
+                Finish();
+        }
+
+        public override void OnFixedUpdate() {
+            ApplyValue();
+
+            if(!everyFrame.Value)
+                Finish();
+        }
+
+        void ApplyValue() {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if(!UpdateCache(go))
+                return;
+
+            cachedComponent.angularVelocity = angle.Value;
+        }
+    }
+}

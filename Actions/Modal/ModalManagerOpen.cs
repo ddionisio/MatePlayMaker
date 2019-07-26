fe27c868b7c39465ea8517af5ba12e8f29abc46b
David Dionisio <ddionisio@renegadeware.com>
2019-07-26 15:54:11 -0400

    add waitClose for ModalManagerOpen

--- Actions/Modal/ModalManagerOpen.cs
+++ Actions/Modal/ModalManagerOpen.cs
@@ -4,17 +4,22 @@ namespace HutongGames.PlayMaker.Actions.M8 {
     [Tooltip("Open a modal.")]
     public class ModalManagerOpen : ModalManagerBase {
         public FsmString modal;
+        [Tooltip("If true, wait for modal to close to finish.")]
+        public FsmBool waitClose;
 
         [CompoundArray("Parameters", "Name", "Object")]
         public FsmString[] paramNames;
         public FsmVar[] paramObjects;
-
+                
         private GenericParams mParams = new GenericParams();
+
+        private ModalManager mMgr;
 
         public override void Reset() {
             base.Reset();
 
             modal = null;
+            waitClose = false;
 
             paramNames = new FsmString[0];
             paramObjects = new FsmVar[0];
@@ -28,11 +33,20 @@ namespace HutongGames.PlayMaker.Actions.M8 {
                     mParams[parmName] = paramObjects[i].GetValue();
             }
 
-            var mgr = manager.GetModalManager(Fsm);
-            if(mgr)
-                mgr.Open(modal.Value, mParams);
+            mMgr = manager.GetModalManager(Fsm);
+            if(mMgr) {
+                mMgr.Open(modal.Value, mParams);
 
-            Finish();
+                if(!waitClose.Value)
+                    Finish();
+            }
+            else
+                Finish();
+        }
+
+        public override void OnUpdate() {
+            if(mMgr == null || !(mMgr.isBusy || mMgr.IsInStack(modal.Value)))
+                Finish();
         }
     }
 }
